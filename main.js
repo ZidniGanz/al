f49e0a0b0c1c7fd31edebea2f65895ac7e9691e7
Zidni Al Azmi <101069904+ZidniGanz@users.noreply.github.com>
2022-12-31 16:19:57 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,60 @@
 const express = require("express");
 const router = express.Router();
-const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-apiKey: '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',
-});
-const openai = new OpenAIApi(configuration);
+const knights = require ('knights-canvas')
 
-const ai = async(q)=>{
-const anu = await openai.createCompletion({
-       model: "text-davinci-003",
-  prompt: q,
-  top_p: 1,
-          frequency_penalty: 0.2,
-          presence_penalty: 0,
-          max_tokens: 400,
-  temperature: 0,});
-          return anu.data.choices[0].text.trim()
-          }
-  router.get("/", async (req, res) => {
-    const text1 = req.query.q
-    const ano = await ai(text1)
-    res.json({text: ano })
-});
+router.get('/', (req, res)=>{
+res.send('ok')})
+
+router.get('/welcome', async(req, res) => {
+  var name = req.query.name
+   var gcname = req.query.gcname
+    var ppgc = req.query.ppgc
+     var member = req.query.member
+      var bg = req.query.bg
+       var pp = req.query.pp
+
+	if (!name) return res.json({ message: 'Masukan parameter name?='})
+  if (!gcname) return res.json({ message: 'Masukan parameter &gcname?='})
+if (!ppgc) return res.json({ message: 'Masukan parameter &ppgc?='})
+if (!bg) return res.json({ message: 'Masukan parameter &bg?='})
+if (!pp) return res.json({ message: 'Masukan parameter &pp?='})
+  if (!member) return res.json({ message: 'Masukan parameter &member?='})
+const image = await new knights.Welcome()
+    .setUsername(name)
+    .setGuildName(gcname)
+    .setGuildIcon(ppgc)
+    .setMemberCount(member)
+    .setAvatar(pp)
+    .setBackground(bg)
+    .toAttachment();
+ data = image.toBuffer();
+    res.set({'Content-Type': 'png'})
+res.send(data)
+})
+router.get('/goodbye', async(req, res) => {
+  var name = req.query.name
+   var gcname = req.query.gcname
+    var ppgc = req.query.ppgc
+     var member = req.query.member
+      var bg = req.query.bg
+       var pp = req.query.pp
+
+	if (!name) return res.json({ message: 'Masukan parameter name?='})
+  if (!gcname) return res.json({ message: 'Masukan parameter &gcname?='})
+if (!ppgc) return res.json({ message: 'Masukan parameter &ppgc?='})
+if (!bg) return res.json({ message: 'Masukan parameter &bg?='})
+if (!pp) return res.json({ message: 'Masukan parameter &pp?='})
+  if (!member) return res.json({ message: 'Masukan parameter &member?='})
+const image = await new knights.Goodbye()
+    .setUsername(name)
+    .setGuildName(gcname)
+    .setGuildIcon(ppgc)
+    .setMemberCount(member)
+    .setAvatar(pp)
+    .setBackground(bg)
+    .toAttachment();
+ data = image.toBuffer();
+    res.set({'Content-Type': 'png'})
+res.send(data)
+  });
 module.exports = router;
